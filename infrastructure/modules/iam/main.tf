c2066336a2f866ba4292362519ed37e9c1bdf2f8
tstojkovski <tomi.stojkovski@gmail.com>
2022-03-08 18:58:27 +0100

    Cloudformation permissions

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -305,6 +305,14 @@ data "aws_iam_policy_document" "role_policy_devops_role" {
     ]
     resources = ["*"]
   }
+  statement {
+    sid = "AllowCloudFormationActions"
+    effect = "Allow"
+    actions = [
+      "cloudformation:*"
+    ]
+    resources = ["*"]
+  }
 }
 
 data "aws_iam_policy_document" "role_policy_ecs_task_role" {
